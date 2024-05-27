3a52563cb823b854c52a9928294f4eee21a4c0b1
Abdelhakam maged <abdelhakmmaged@gmail.com>
2024-05-28 00:36:38 +0300

    .

--- src/componant/blog/pagetwo/pagetwo-ourclasses/pagetwo-ourclasses.js
+++ src/componant/blog/pagetwo/pagetwo-ourclasses/pagetwo-ourclasses.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import { FaRunning, FaLongArrowAltRight } from "react-icons/fa";
+import React from "react";
 
-import "./pagetwo-ourclasses.css"
+import "./pagetwo-ourclasses.css";
 
-
-import Tranning from './tranning-type';
-import Fitenssicon from '../../../fitenssicon/fitenssicon';
-
-
-
-
+import Tranning from "./tranning-type";
+import Fitenssicon from "../../../fitenssicon/fitenssicon";
 
 const PagetwoOurclasses = () => {
   return (
-    <div className='mt-5 pagetwo-ourclasses'>
-      <div className=' up-side'>
-          <div className="componantheader m-auto text-center text-light">
-
-
-
+    <div className="mt-5 pagetwo-ourclasses">
+      <div className=" up-side">
+        <div className="componantheader m-auto text-center text-light">
           <div className=" textcolor fw-bold ">
             <span className="headerspace mb-1"></span> OUR CLASSES
           </div>
@@ -26,21 +17,22 @@ const PagetwoOurclasses = () => {
           <h3 className="fs-2">OUR BEST FITNESS & BODYBUILDING CLASSES </h3>
         </div>
 
-
-
         <div className="d-flex align-items-center justify-content-center pb-3 pt-3 flex-wrap ">
-     <Tranning h2={"ayhaga"} text={"GYM" } background={'bagrimgone'}/>
-     <Tranning h2={"ayhaga"} text={"GYM" } background={'bagrimgtwo'}/>
-     <Tranning h2={"ayhaga"} text={"GYM" } background={'bagrimgthree'}/>
-     <Tranning h2={"ayhaga"} text={"GYM" } background={'bagrimgfour'}/>
+          <Tranning h2={"ayhaga"} text={"GYM"} background={"bagrimgone"} />
+          <Tranning h2={"ayhaga"} text={"GYM"} background={"bagrimgtwo"} />
+          <Tranning h2={"ayhaga"} text={"GYM"} background={"bagrimgthree"} />
+          <Tranning h2={"ayhaga"} text={"GYM"} background={"bagrimgfour"} />
         </div>
 
-     <Fitenssicon contanerclass={"underside-center"} bodyclass={"pagetwo-ourclasses-underside"} texcol={'text-light'} iconclass={"fiticoncolor"} />
-    
-        </div>
-      
+        <Fitenssicon
+          contanerclass={"underside-center"}
+          bodyclass={"pagetwo-ourclasses-underside"}
+          texcol={"text-light"}
+          iconclass={"fiticoncolor"}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default PagetwoOurclasses;
